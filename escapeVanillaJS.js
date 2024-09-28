675e7ffa18b861129bd74a643a6ad66c20d60d78
Khanyisa Maluleka <khanyisamaluleka20@gmail.com>
2024-09-28 23:42:29 +0200

    Added await before promise

--- escapeVanillaJS.js
+++ escapeVanillaJS.js
@@ -20,17 +20,17 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     // 🪲 Bug: Asynchronous function ?
-    document.getElementById("solveRoom3").addEventListener("click", () => {
-        fetch('directions.json') 
-            .then(response => response.json())
-            .then(directions => {
-                navigateLabyrinth(directions)
-                    .then(message => {
-                        // 🪲 Bug: Incorrect method
-                        document.getElementById("room3Result").innerHTML = message;
-                    });
-            });
+    document.getElementById("solveRoom3").addEventListener("click", async () => {
+        // Add 'await' before fetch and navigating labyrinth
+        const response = await fetch('directions.json');
+        const directions = await response.json();
+        
+        // Await the result of the asynchronous function
+        const message = await navigateLabyrinth(directions);
+        
+        document.getElementById("room3Result").innerHTML = message;
     });
+    
 });
 
 function findMostRecentBook(books) {
